d45bb366617d75022097d18d7b696805acc64c79
Fuwn <contact@fuwn.me>
2022-03-09 03:01:28 -0800

    feat(senpy): 0.1.0 :star:

---
+++ build.rs
@@ -0,0 +1,43 @@
+// This file is part of senpy-rs <https://github.com/senpy-club/senpy-rs>.
+// Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
+// SPDX-License-Identifier: GPL-3.0-only
+
+/// <https://github.com/denoland/deno/blob/main/cli/build.rs#L265:L285>
+fn git_commit_hash() -> String {
+  if let Ok(output) = std::process::Command::new("git")
+    .arg("rev-list")
+    .arg("-1")
+    .arg("HEAD")
+    .output()
+  {
+    if output.status.success() {
+      std::str::from_utf8(&output.stdout[..40])
+        .unwrap()
+        .to_string()
+    } else {
+      "UNKNOWN".to_string()
+    }
+  } else {
+    "UNKNOWN".to_string()
+  }
+}
+
+fn main() {
+  println!("cargo:rerun-if-changed=build.rs");
+  println!("cargo:rustc-env=GIT_COMMIT_HASH={}", git_commit_hash());
+  println!("cargo:rerun-if-env-changed=GIT_COMMIT_HASH");
+}
